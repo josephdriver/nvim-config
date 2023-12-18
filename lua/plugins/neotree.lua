fce5406e723ccd5cf735edb56071fe9094ea104f
Joe Driver <joseph.driver0@gmail.com>
2023-12-18 20:54:38 +1000

    added autopairs, nvim-surround, toggle-term

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,16 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    local neo_tree = require('neo-tree')
+    neo_tree.setup{
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false
+        }
+      }
+    }
+
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
   end
 }
